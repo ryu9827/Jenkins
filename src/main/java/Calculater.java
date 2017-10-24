8f8bb29616b23685ea993b3462911eeb0c4eee07
Bruce Li <ryu9827@gmail.com>
2017-10-25 11:51:13 +1300

    first commit

---
+++ src/main/java/Calculater.java
@@ -0,0 +1,16 @@
+
+public class Calculater {
+
+	public static void main(String[] args) {
+		
+		int x,y;
+		x=3;
+		y=4;
+		add(x,y);
+
+	}
+	public static int add (int x, int y) {
+		return x+y;
+			
+		}
+}
---
+++ src/test/java/CalculaterTest.java
@@ -0,0 +1,23 @@
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+public class CalculaterTest extends TestCase {
+	Calculater obj;
+	int x,y;
+	
+	protected void setUp() throws Exception {
+		
+		x=3;
+		y=4;
+		obj = new Calculater();
+	}
+	public void testAdd() {
+		assertEquals(7, obj.add(x, y));
+		
+	}
+	protected void tearDown() throws Exception {
+		x=0;
+		y=0;
+	}
+
+}
